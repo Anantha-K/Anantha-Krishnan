e8390d48abc0b20613d43a5fce0dfb125b80a4e8
Anantha Krishnan <jananthak@gmail.com>
2024-02-18 18:35:11 +0530

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ const page = () => {
         <div className="bg-black w-full flex items-end justify-center h-[75%] text-white">
           <div className="flex flex-col">
 
-          <Image src={img} width={500} height={500} className='z-0 translate-y-32'></Image>
+          {/* <Image src={img} width={500} height={500} className='z-0 translate-y-32'></Image> */}
           <div className="flex-col items-center -translate-y-10 z-10 justify-center">
             <h1 className="text-6xl">Anantha Krishnan</h1>
             <div className="flex justify-center text-3xl my-5">
@@ -32,7 +32,17 @@ const page = () => {
 
 
 
+        {/* Projects */}
+        <div className="bg-green-400 w-full flex flex-col items-center ">
+          <h1 className="text-4xl">
+            Projects
+            </h1>
+            {/* cards */}
+            <div>
+              hi
+            </div>
 
+        </div>
 
 
         <footer className="bg-red-500 h-48 w-full text-white items-center flex flex-col justify-between bottom-0">
