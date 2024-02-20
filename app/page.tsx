32c1b20a4f49cafd0acef83f712c27139a86c4b0
Anantha Krishnan <jananthak@gmail.com>
2024-02-20 18:02:57 +0530

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,11 @@ const page = () => {
   return (
     <>
       <div className="h-screen w-full">
-        <div className="bg-black w-full flex items-end justify-center h-[75%] text-white">
+        <div className="bg-black w-full flex items-end justify-center h-screen text-white">
           <div className="flex flex-col">
             {/* <Image src={img} width={500} height={500} className='z-0 translate-y-32'></Image> */}
             <div className="flex-col items-center -translate-y-10 z-10 justify-center">
-              <h1 className="text-6xl">Anantha Krishnan</h1>
+              <h1 className="text-8xl">Anantha Krishnan</h1>
               <div className="flex justify-center text-3xl my-5">
                 <Typewriter
                   options={{
@@ -42,8 +42,9 @@ const page = () => {
 
 
                   {/* Tech Stack */}
-        <div className="w-full flex bg-purple-300 justify-center text-3xl items-center">
-                  <h1>Tech Stack<span>.</span></h1>
+        <div className="w-full flex bg-purple-300 justify-center flex-col text-3xl items-center">
+                  <h1 className="text-5xl">Tech Stack<span>.</span></h1>
+
         </div>
         {/* Projects */}
         <div className="bg-green-400 w-full flex flex-col items-center ">
@@ -52,7 +53,7 @@ const page = () => {
           <div>hi</div>
         </div>
 
-        <footer className=" h-48  w-full text-white items-center flex-col flex align-middle justify-between bottom-0">
+        <footer className=" h-72  w-full text-white items-center bg-black flex-col flex align-middle justify-between bottom-0">
           <Link href="#" className="button mt-5 border-2 border-white">
             <svg viewBox="0 0 384 512" className="svgIcon">
               <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"></path>
