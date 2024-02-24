bc18cc33380a81d0dcfe3a0ee5ec16338000775f
Anantha Krishnan <jananthak@gmail.com>
2024-02-24 21:21:42 +0530

    changes

---
+++ app/What/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <div className='bg-red-50 w-full h-96'>
+        <h1>
+            What I'm Good At
+        </h1>
+
+    </div>
+    </>
+
+    )
+}
+
+export default page
