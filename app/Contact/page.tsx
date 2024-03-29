d77cff17af05c21cc8ea2f87ede94ac8a2678034
Anantha Krishnan <jananthak@gmail.com>
2024-03-29 23:15:48 +0530

    contact

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 const page = () => {
+  const handleSubmit=()=>{
+    
+  }
   return (
-    <div>page</div>
+    <div className='bg-black w-full min-h-screen'>
+      <h1>Contact Me</h1>
+
+      <button onClick={handleSubmit}>Send</button>
+
+    </div>
   )
 }
 
