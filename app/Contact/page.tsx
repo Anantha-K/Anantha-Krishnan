211dc9a957c621b900822b7aa548bec528907264
Anantha Krishnan <jananthak@gmail.com>
2024-06-07 19:40:03 +0530

    fixx

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -3,20 +3,3 @@ import React from 'react'
 const page = () => {
   return (
     <>
-    <div>Contact
-      <form>
-        <input type="name">Name</input>
-        <input type="email">email</input>
-      </form>
-
-
-        hi
-    </div>
-    <div>
-        hgh
-    </div>
-    </>
-  )
-}
-
-export default page
